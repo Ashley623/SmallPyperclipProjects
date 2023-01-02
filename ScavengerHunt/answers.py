f9faa34233336f7489458970168bb50597d209fb
Ashley623 <dolphieashley@gmail.com>
2023-01-02 14:24:50 -0600

    Added ScavengerHunt mini project

---
+++ ScavengerHunt/answers.py
@@ -0,0 +1,23 @@
+# SPOILERS AHEAD!! START THE SCAVENGER HUNT FROM THE scavengerHunt.py FILE!!
+
+# The questions used for the scavenger hunt
+questions = [
+    "What is the first sentence of the readme for the repository this is in?",
+    "What is the github username for the creator of this project?",
+    "What is the first sentence of the documentation for pyperclip?",
+    "What year was github founded?",
+    "What is the heading on the github about page?",
+    "What is the mission of the python software foundation?",
+    "What would you need to code to obtain the information on a user's clipboard?"
+]
+
+# The answers cooresponding to the questions above for the scavenger hunt
+answers = [
+    "This is a collection of small projects involving the pyperclip library in Python",
+    "Ashley623",
+    "Pyperclip provides a cross-platform Python module for copying and pasting text to the clipboard",
+    "2008",
+    "Let's build from here",
+    "The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers",
+    "pyperclip.copy()"
+]
---
+++ ScavengerHunt/scavengerHunt.py
@@ -0,0 +1,28 @@
+import pyperclip as pc
+from answers import answers
+from answers import questions
+# The answers.py file has all the questions and the answers, so if you look at this file you will not get spoilers
+
+points = 0
+print("For this scavenger hunt game, copy the answer to the question on to your clipboard")
+print("You have 3 tries to copy the correct answer, and the number of points starts at 3 and decreases each try")
+for i in range(len(questions)):
+    # Print the question
+    print(questions[i])
+    correct = False
+    for j in range(3): # Loops through the 3 different tries for each question
+        pc.waitForNewPaste() # This pauses the program until the user copies something new
+        if(pc.paste().strip()[:len(answers[i])] == answers[i] or pc.paste().strip()[-len(answers[i]):] == answers[i]):
+            # The copied text is striped, and then only the beginning part or the last part are looked at in case
+            #   the user copies extra characters aside from the answer (like a period)
+            print("You got it correct!!")
+            correct = True
+            points += 3-j # Number of points recieved decreases by 1 each attempt
+            break
+    if(not correct):
+        # Prints out the correct answer if you didn't get it in 3 tries
+        print("You failed to copy the correct answer with your 3 attempts")
+        print("The answer was:",answers[i])
+        j = 3
+    print("You got", 3-j, "points, your total score is", points)
+    print("\n")
